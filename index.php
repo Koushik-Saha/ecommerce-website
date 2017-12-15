7b3ae665589590c52fbdbc1809b0d968d1a050e8
Koushik <Koushik.saha666@gmail.com>
2017-12-15 21:41:59 +0600

    Updated Database With Products Details

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE>
+<?php
+
+include("functions/functions.php");
+
+?>
+<html>
+	<head>
+		<title>
+			Online Cow Provider:
+		</title>
+
+		<link rel="stylesheet" href="style.css" media="all" />
+	</head>
+<body>
+
+	<div class="main_wrapper">
+
+		<div class="header_wrapper">
+
+			<img id="logo" src="images/cow.gif" />
+			<img id="banner" src="images/banner.gif" />
+			
+		</div>
+
+		<div class="menubar">
+			<ul id="menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">All Products</a></li>
+				<li><a href="#">My Account</a></li>
+				<li><a href="#">Sign Up</a></li>
+				<li><a href="#">Shopping Cart</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+
+			<div id="form">
+				<form method="get" action="results.php" enctype="multipart/form-data">
+					<input type="text" name="user_query" placeholder="Search a Products" />
+
+					<input type="submit" name="search" value="Search" />
+				 </form>
+				
+			</div> 
+		</div>
+
+
+
+		<div class = "content_wrapper">
+
+			<div id="sidebar">
+				
+				<div id="sidebar_title">Categories</div>
+
+				<ul id="cats">
+
+					<?php getCats(); ?>
+					
+				</ul>
+			
+				<div id="sidebar_title">Brands</div>
+
+				<ul id="cats">
+
+				<?php getBrands(); ?>
+					
+				</ul>
+
+				<div id="sidebar_title">Price</div>
+
+				<ul id="cats">
+
+					<?php getPrice(); ?>
+
+				</ul>
+
+
+		</div>
+
+
+			<div id = "content_area">This is content area</div>
+
+		</div>
+
+		<div id="footer">
+			
+			<h2 style="text-align: center;
+			padding-top: 30px;">&copy; 2017 by www.KoushikSaha.com </h2>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
