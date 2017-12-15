83af2fb920c645d1a5f7297c785d21def15caff2
Koushik <Koushik.saha666@gmail.com>
2017-12-15 22:13:08 +0600

    Upload Admin Image and details about products in database not show in index page

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -124,6 +124,10 @@ include("includes/db.php");
 		$product_image_tmp = $_FILES['product_image']['tmp_name'];
 
 
+		move_uploaded_file($product_image_tmp, "product_images/$product_image");
+
+
+
 		$insert_product = "insert into products (product_cat,product_brand,product_title,product_price,product_desc,product_image,product_keywords) values ('$product_cat','$product_brand', '$product_title', '$product_price', '$product_desc', '$product_image', '$product_keywords')";
 
 
