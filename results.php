c03e854501b185810d78182ed08353c8f71cb47c
Koushik <Koushik.saha666@gmail.com>
2017-12-21 13:30:37 +0600

    Search engine

--- results.php
+++ results.php
@@ -94,7 +94,7 @@ include("functions/functions.php");
 
 						$search_query = $_GET['user_query'];
 
-					$get_pro = "select * from products where product_keywords like '%$search_query%' ";
+					$get_pro = "select * from products where product_keywords like '%$search_query%'    ";
 
 					$run_pro = mysqli_query($con, $get_pro);
 
