442897bbe92a29cf077645dbdbcac06d2e4e8b10
Koushik <Koushik.saha666@gmail.com>
2017-12-25 13:18:27 +0600

    Update Cart Table

--- cart.php
+++ cart.php
@@ -140,7 +140,7 @@ include("functions/functions.php");
 							?>
 
 							<tr align="center">
-								<td><input type="checkbox" name="remove[]" /></td>
+								<td><input type="checkbox" name="remove[]" value="<?php echo $pro_id; ?>" /></td>
 								<td><?php echo $product_title; ?> <br>
 									<img src="admin_area/product_images/<?php echo $product_image; ?>" width = "60" height = "60" />
 								</td>
@@ -150,17 +150,48 @@ include("functions/functions.php");
 
 						<?php } } ?>
 
-						<tr align="right">
-								<td colspan="4"><b> Sub Total:</b></td>
+						<tr>
+								<td colspan="4" align="right"><b> Sub Total:</b></td>
 								<td>
 									<?php echo "৳" . $total; ?>
 								</td>
 
-							</tr>
-							
+								<tr align="center">
+									<td colspan="2"><input type="submit" name="update_cart" value="Update Cart" /></td>
+									<td><input type="submit" name="continue" value="Continue Shopping" /></td>
+									<td><button><a href="checkout.php" style="text-decoration: none; color: black; ">Checkout</a></button></td>
+								</tr>
+
 						</table>
 						
 					</form>
+
+					<?php
+
+						$ip = getIp();
+
+						if (isset($_POST['update_cart'])){
+
+							foreach ($_POST['remove'] as $remove_id) {
+
+								$delete_product = "delete from cart where p_id='$remove_id' AND ip_add='$ip'";
+
+								$run_delete = mysqli_query($con, $delete_product);
+
+								if($run_delete){
+
+									echo "<script>window.open('cart.php', '_self')</script";
+								}
+								
+							}
+						}
+
+						if(isset($_POST['continue'])){
+
+							echo "<script>window.open('index.php', '_self')</script";
+
+						}
+					?>
 				</div>
 
 
