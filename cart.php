72baca99db31fe1192fb2c784ef7d0ca192f6872
Koushik <Koushik.saha666@gmail.com>
2017-12-27 19:05:46 +0600

    Customer Session

--- cart.php
+++ cart.php
@@ -85,8 +85,32 @@ include("functions/functions.php");
 
 				<div id="shopping_cart">
 
-					<span style="float: right; font-size: 18px; padding: 5px; line-height: 40px;">
-						Welcome Guest! <b style="color: yellow">Shopping Cart -</b>Total Items: <?php total_items();?> Total Price: <?php total_price(); ?><a href="cart.php" style="color: yellow"> Go to Cart</a>
+					<span style="float: right; font-size: 17px; padding: 5px; line-height: 40px;">
+
+						<?php 
+
+							if(isset($_SESSION['customer_email'])){
+
+								echo "<b>Welcome:</b>" . $_SESSION['customer_email'] . "<b style='color:yellow;'>Your</b>";
+							}else{
+
+								echo "<b>Welcome Guest:</b>";
+							}
+						?>
+					
+
+						 <b style="color: yellow">Shopping Cart -</b>Total Items: <?php total_items();?> Total Price: <?php total_price(); ?><a href="index.php" style="color: yellow"> Back to Shop</a>
+
+						<?php
+
+							if(!isset($_SESSION['customer_email'])){
+
+								echo "<a href='checkout.php' style='color:orange';>Login</a>";
+							}else{
+
+								echo "<a href='logout.php' style='color:orange';>Logout</a>";
+							}
+						?>
 					</span>
 					
 				</div>
@@ -187,7 +211,7 @@ include("functions/functions.php");
 
 					<?php
 
-					// function updatecart(){
+					 function updatecart(){
 
 						global $con;
 
@@ -215,9 +239,9 @@ include("functions/functions.php");
 
 						}
 
-						// echo @$up_cart = updatecart();
+						 echo @$up_cart = updatecart();
 
-					// }
+					 }
 					?>
 				</div>
 
--- checkout.php
+++ checkout.php
@@ -85,8 +85,19 @@ include("functions/functions.php");
 
 				<div id="shopping_cart">
 
-					<span style="float: right; font-size: 18px; padding: 5px; line-height: 40px;">
-						Welcome Guest! <b style="color: yellow">Shopping Cart -</b>Total Items: <?php total_items();?> Total Price: <?php total_price(); ?><a href="cart.php" style="color: yellow"> Go to Cart</a>
+					<span style="float: right; font-size: 17px; padding: 5px; line-height: 40px;">
+
+						<?php 
+
+							if(isset($_SESSION['customer_email'])){
+
+								echo "<b>Welcome:</b>" . $_SESSION['customer_email'] . "<b style='color:yellow;'>Your</b>";
+							}else{
+
+								echo "<b>Welcome Guest:</b>";
+							}
+						?>
+						<b style="color: yellow">Shopping Cart -</b>Total Items: <?php total_items();?> Total Price: <?php total_price(); ?><a href="index.php" style="color: yellow"> Back to Shop</a>
 					</span>
 					
 				</div>
